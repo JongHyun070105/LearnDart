4643666309599f23c4231d5862eac5e662ab01d8
JongHyun070105 <bobi09361@gmail.com>
2025-06-18 19:15:43 +0900

    Learn basic_type (String, bool, int, double, num)

---
+++ .idea/codes/basic_type.dart
@@ -0,0 +1,11 @@
+void main(){
+  String name = '종현'; // '' 쓰든 "" 쓰든 상관이 없다
+  bool isnull = false;
+  int age = 19;
+  double money = 12.82;
+
+  // num을 상속 받은게 int 와 double이 있는데 이 num으로 변수를 구성하면 값이 int가 될 수도 있고 dobule이 될 수도 있다.
+  // num은 많이 사용안한다
+  num x = 12;
+  x = 1.2;
+}
