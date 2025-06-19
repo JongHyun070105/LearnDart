01c08788d11d083654bae0062d54ee1fde61ad2b
JongHyun070105 <bobi09361@gmail.com>
2025-06-19 16:35:41 +0900

    Learn string_interpolation

--- .idea/codes/string_interpolation.dart
+++ .idea/codes/string_interpolation.dart
@@ -1,8 +1,9 @@
 void main(){
   // text에 변수를 추가하는 방법이다
-
   var name = '박종현';
-  var greeting = '안녕하세요. 제 이름은 $name이고, 만나서 반가워요';
+  var age = 17;
+
+  var greeting = '안녕하세요. 제 이름은 $name이고, 제 나이는 ${age + 2}입니다.'; // $ 뒤에 변수를 사용 | {}묶으면 계산도 가능
 
   print(greeting);
 }
