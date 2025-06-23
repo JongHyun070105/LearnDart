0da8aa1e44f01a6c706f23ee65d946b0e7a6d3ce
JongHyun070105 <bobi09361@gmail.com>
2025-06-23 20:23:13 +0900

    Learn named_parameter

--- .idea/codes/named_parameter.dart
+++ .idea/codes/named_parameter.dart
@@ -5,9 +5,15 @@ void main(){
   print(sayHello(
   age: 19,
   name: '박종현',
-  country: '사우스 코리아'));
+  country: '한국'));
 }
 
-String sayHello({String name, int age, String country}){ // 중괄호로 감싸면 named parameter를 사용할 수 있다
+// 중괄호로 감싸면 named parameter를 사용할 수 있다
+String sayHello({
+  required String name, // 호출될 때 반드시 name을 가져와야하게 만듦
+  int age = 0,
+  String country = '지구'
+}){
+  // Default Value도 정할 수 있다. 유저가 빈값을 보냈을 경우를 대비한다. 변수 = 원하는 값 or required 를 사용할 수 있다.
   return "안녕 내 이름은 $name, 내 나이는 $age고, 내가 태어난 나라는 $country야.";
 }
