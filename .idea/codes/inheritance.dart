d9fa0ba76f5f807061c10e7a58ec98d0e85065c6
JongHyun070105 <bobi09361@gmail.com>
2025-07-30 19:32:20 +0900

    Learn inheritance

--- .idea/codes/inheritance.dart
+++ .idea/codes/inheritance.dart
@@ -1,1 +1,28 @@
-void main() {}
+class Human {
+  final String name;
+
+  Human({required this.name});
+
+  void sayHello() {
+    print('hi my name is $name');
+  }
+}
+
+enum Team { blue, red }
+
+class Player extends Human {
+  final Team team;
+
+  Player({required this.team, required String name}) : super(name: name);
+
+  @override
+  void sayHello() {
+    super.sayHello();
+    print('and I play for ${team}');
+  }
+}
+
+void main() {
+  var player = Player(team: Team.red, name: 'jong hyun');
+  player.sayHello();
+}
