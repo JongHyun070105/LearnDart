276441be3cb5b707719903a2f132068325be9684
JongHyun070105 <bobi09361@gmail.com>
2025-07-30 19:42:57 +0900

    Learn mixin

--- .idea/codes/mixin.dart
+++ .idea/codes/mixin.dart
@@ -1,1 +1,31 @@
-void main() {}
+mixin Strong {
+  final double strLevel = 1500.99;
+}
+
+mixin QuickRunner {
+  void runQuik() {
+    print('Runnnnnnn!!');
+  }
+}
+
+mixin Tall {
+  final double height = 1.99;
+}
+
+class Horse with Strong, QuickRunner {}
+
+class Kid with QuickRunner {}
+
+enum Team { blue, red }
+
+class Player with Strong, QuickRunner, Tall {
+  final Team team;
+
+  Player({required this.team});
+}
+
+void main() {
+  var player = Player(team: Team.red);
+
+  player.runQuik();
+}
